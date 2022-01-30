46d09a2d57714520451876120ce7904e1d1b6d4b
Emaroca <trassaniemmanuel@gmail.com>
2022-01-30 20:00:18 -0300

    responsive

--- client/src/responsive.js
+++ client/src/responsive.js
@@ -1,12 +1,5 @@
 import { css } from "styled-components";
 
-// export const mobile = (props) => {
-//   return css`
-//     @media only screen and (max-width: 414px) {
-//       ${props}
-//     }
-//   `;
-// };
 export const mobile = (props) => {
   return css`
   @media only screen and (min-width : 320px) and (max-width : 630px) {
