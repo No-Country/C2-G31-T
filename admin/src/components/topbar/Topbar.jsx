fe31c59b2a44b75fe16795ba0ce25c83368ce495
pabloc33 <pablo.contino33@gmail.com>
2022-01-31 20:54:00 -0300

    cambios pendientes guardados

---
+++ admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./topbar.css";
+import {
+  NotificationsNone,
+  Language,
+  // Settings,
+  ExitToApp,
+} from "@material-ui/icons";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/userRedux";
+import { persistor } from "../../redux/store";
+import { useHistory } from "react-router-dom";
+
+export default function Topbar() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleLogout = () => {
+    persistor.purge();
+    dispatch(logout());
+    history.push("/");
+  };
+
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">adminpage</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <ExitToApp onClick={handleLogout} />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
