77b39a670477a8616203061c4dda02590c1f34fa
pabloc33 <pablo.contino33@gmail.com>
2022-01-31 21:01:35 -0300

    cambio en la url

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -19,8 +19,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://c2-31-t.herokuapp.com/api/products?category=${cat}` //aquí hay un problema
+            : "https://c2-31-t.herokuapp.com/api/products"
         );
         setProducts(res.data);
       } catch (error) {}
